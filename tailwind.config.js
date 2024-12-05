48a5097d50d86c8b482825e041266f8ecdfd14db
Mbarga <emmanuelmbarg@gmail.com>
2024-12-05 17:22:31 +0100

    testgenuka travail initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,16 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        MiniPortable: { max: "288px" },
+        mobil: { min: "289px", max: "480px" },
+        Tablette: { min: "481px", max: "761px" },
+        Laptop: { min: "762px", max: "1025px" },
+        screenLarge: { min: "1026px" },
+      },
+      fontFamily:{
+        "Outfit":['Outfit','sans-serif'],
+      },
     },
   },
   plugins: [],
