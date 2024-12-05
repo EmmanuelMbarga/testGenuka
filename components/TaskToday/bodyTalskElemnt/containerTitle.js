fd87d12d27e46ff3775764339fa1d758777cc850
Mbarga <emmanuelmbarg@gmail.com>
2024-12-05 19:37:51 +0100

    creation des composant de titre des taches

---
+++ components/TaskToday/bodyTalskElemnt/containerTitle.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Style from "../../../styles/talskDay.module.css"
+
+
+
+export default function ContainerTitle() {
+  return (
+    <div>
+      <h1 className={`${Style.textTitelTask}`}>today's talsk</h1>
+      <h2 className={`${Style.subTitleTask}`}>wednesday, 11 May</h2>
+    </div>
+  );
+}
